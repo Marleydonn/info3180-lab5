e8035d028808d82e65332a1f26aee3d0666e0129
Jahmarley Robinson <jahmarley2002@gmail.com>
2024-04-07 16:19:15 -0500

    Completed Lab

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import MoviesView from '../views/MoviesView.vue' // import MoviesView component
+
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -16,6 +18,16 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue')
+    },
+    {
+      path: '/movies/create',
+      name: 'add-movie',
+      component: () => import('../views/AddMovieFormView.vue')
+    },
+    {
+      path: '/movies',
+      name: 'movies',
+      component: MoviesView // add new route for MoviesView component
     }
   ]
 })
